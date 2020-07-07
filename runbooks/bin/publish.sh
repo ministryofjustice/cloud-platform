9d2ea598417012856fd420bb6d39356c9326e0df
David Salgado <david@digitalronin.com>
2020-07-07 16:02:35 +0100

    Add a github action publishing workflow
    This is a slightly adapted version of the user-
    guide publishing workflow.
    
    Currently, it publishes to the 
    cloud-platform-runbooks2 repo, with the published
    site hosted at
    https://runbooks2.cloud-platform.service.justice.gov.uk
    
    I'll change those to the correct values once the
    publishing process is working correctly.

---
+++ runbooks/bin/publish.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# This script is called by .github/workflows/publish.yml after compiling the source documents into
+# HTML files in the `docs` directory.
+#
+# It adds and pushes the compiled files to a 'publishing' repo, where they are served as a website via
+# github pages.
+
+set -euo pipefail
+
+# Check for internal and external broken links
+bundle exec htmlproofer ./docs --http-status-ignore 429 --allow-hash-href --url-swap "https?\:\/\/runbooks\.cloud-platform\.service\.justice\.gov\.uk:" ./docs
+
+# PUBLISHING_GIT_TOKEN is a secret in the source repository. It should contain a github personal access token
+# with `public_repo` scope, and MoJ SSO enabled.
+REPOSITORY_PATH="https://${PUBLISHING_GIT_TOKEN}@github.com/$GITHUB_PAGES_REPO_OWNER/$GITHUB_PAGES_REPO_NAME.git"
+
+echo "Checkout the publish repo"
+git clone $REPOSITORY_PATH
+
+echo "Add compiled files"
+cp -R docs/* $GITHUB_PAGES_REPO_NAME
+
+cd $GITHUB_PAGES_REPO_NAME
+
+echo "Push the changes"
+git config --global user.email "${GITHUB_PAGES_REPO_AUTHOR_EMAIL}"
+git config --global user.name "${GITHUB_PAGES_REPO_AUTHOR}"
+git add .
+git commit -m "Published at $(date)"
+git push
+
+echo "Publishing complete"
