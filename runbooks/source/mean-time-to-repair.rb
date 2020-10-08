9152dd21fe363484854a8acb8d88f3c2ead2287a
David Salgado <david@digitalronin.com>
2020-10-08 09:51:55 +0100

    Tweak code layout

--- runbooks/source/mean-time-to-repair.rb
+++ runbooks/source/mean-time-to-repair.rb
@@ -16,67 +16,11 @@ TIME_TO_REPAIR_REGEX = %r[- ..Time to repair..: (\d+h \d+m)]
 TIME_TO_RESOLVE_REGEX = %r[- ..Time to resolve..: (\d+h \d+m)]
 TIME_REGEX = %r[(\d\d\d\d-\d\d-\d\d \d\d:\d\d)]
 
-class Quarter
-  attr_reader :title, :incidents
-
-  def initialize(title, incidents)
-    @title = title
-    @incidents = incidents
-      .reject { |i| i == {} }
-      .map { |i| Incident.new(i) }
-  end
-
-  def report
-    <<~EOF
-    #{title}
-      Incidents: #{incidents.length}
-      Mean time to repair: #{mean_time_to_repair}
-      Mean time to resolve: #{mean_time_to_resolve}
-    EOF
-  end
-
-  private
-
-  def mean_time_to_repair
-    sum = incidents.map(&:time_to_repair).sum
-    hours_and_minutes(sum / incidents.length)
-  end
-
-  def mean_time_to_resolve
-    sum = incidents.map(&:time_to_resolve).sum
-    hours_and_minutes(sum / incidents.length)
-  end
-
-  def hours_and_minutes(i)
-    hours = i / 3600
-    seconds = i % 3600
-    minutes = seconds / 60
-    "#{hours}h #{minutes}m"
-  end
-end
-
-class Incident
-  def initialize(params)
-    @time_to_repair = params[:time_to_repair]
-    @time_to_resolve = params[:time_to_resolve]
-  end
-
-  def time_to_repair
-    to_seconds(@time_to_repair || @time_to_resolve) # In case one is missing
-  end
-
-  def time_to_resolve
-    to_seconds(@time_to_resolve || @time_to_repair) # In case one is missing
-  end
-
-  private
-
-  def to_seconds(str)
-    hours, minutes = str.split(" ").map(&:to_i)
-    (hours * 3600) + (minutes * 60)
-  rescue
-    binding.pry
-  end
+def main
+  puts parse_incident_log
+    .map { |quarter, incidents| Quarter.new(quarter, incidents) }
+    .sort { |a, b| a.title <=> b.title }
+    .map(&:report)
 end
 
 def parse_incident_log
@@ -116,7 +60,70 @@ def time_from_line(line)
   end
 end
 
-puts parse_incident_log
-  .map { |quarter, incidents| Quarter.new(quarter, incidents) }
-  .sort { |a, b| a.title <=> b.title }
-  .map(&:report)
+
+class Quarter
+  attr_reader :title, :incidents
+
+  def initialize(title, incidents)
+    @title = title
+    @incidents = incidents
+      .reject { |i| i == {} }
+      .map { |i| Incident.new(i) }
+  end
+
+  def report
+    <<~EOF
+    #{title}
+      Incidents: #{incidents.length}
+      Mean time to repair: #{mean_time_to_repair}
+      Mean time to resolve: #{mean_time_to_resolve}
+    EOF
+  end
+
+  private
+
+  def mean_time_to_repair
+    sum = incidents.map(&:time_to_repair).sum
+    hours_and_minutes(sum / incidents.length)
+  end
+
+  def mean_time_to_resolve
+    sum = incidents.map(&:time_to_resolve).sum
+    hours_and_minutes(sum / incidents.length)
+  end
+
+  def hours_and_minutes(i)
+    hours = i / 3600
+    seconds = i % 3600
+    minutes = seconds / 60
+    "#{hours}h #{minutes}m"
+  end
+end
+
+
+class Incident
+  def initialize(params)
+    @time_to_repair = params[:time_to_repair]
+    @time_to_resolve = params[:time_to_resolve]
+  end
+
+  def time_to_repair
+    to_seconds(@time_to_repair || @time_to_resolve) # In case one is missing
+  end
+
+  def time_to_resolve
+    to_seconds(@time_to_resolve || @time_to_repair) # In case one is missing
+  end
+
+  private
+
+  def to_seconds(str)
+    hours, minutes = str.split(" ").map(&:to_i)
+    (hours * 3600) + (minutes * 60)
+  rescue
+    binding.pry
+  end
+end
+
+main
+
