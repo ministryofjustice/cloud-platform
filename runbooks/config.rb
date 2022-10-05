9f946477153d93390201bd5c63dcee40ef54f8c9
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2022-10-05 16:15:20 +0100

    Migrate runbooks to use template-docs site

--- runbooks/config.rb
+++
@@ -1,8 +0,0 @@
-# NOTE: Changes to this file require rebuilding the docker image.
-#       Otherwise, your change will not affect the live site.
-
-require 'govuk_tech_docs'
-
-GovukTechDocs.configure(self)
-
-set :layout, 'custom'
