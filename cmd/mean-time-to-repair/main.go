57c88dec5566e14c9009d1c353d78525369aaa77
Mike Bell <mike.bell1@digital.justice.gov.uk>
2024-10-09 09:32:55 +0100

    feat: add new go mean time to repair script

---
+++ cmd/mean-time-to-repair/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func convertToRaw(data string) int {
+	s := strings.Split(data, " ")
+	total := 0
+
+	hours := 0
+	minutes := 0
+
+	for _, time := range s {
+		if strings.Contains(time, "h") {
+			time = strings.Replace(time, "h", "", -1)
+			i, err := strconv.Atoi(time)
+			if err != nil {
+				panic(err)
+			}
+			hours = convertHoursToMinutes(i)
+		}
+		if strings.Contains(time, "m") {
+			time = strings.Replace(time, "m", "", -1)
+			i, err := strconv.Atoi(time)
+			if err != nil {
+				panic(err)
+			}
+			minutes = i
+		}
+	}
+	total = hours + minutes
+
+	return total
+}
+
+func convertHoursToMinutes(i int) int {
+	i = i * 60
+	return i
+}
+
+func main() {
+	lines, err := readLines("../../runbooks/source/incident-log.html.md.erb")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	var data []string
+
+	var str strings.Builder
+
+	for _, line := range lines {
+		str.WriteString(line)
+
+		re := regexp.MustCompile(`---`)
+		match := re.FindString(line)
+
+		if match != "" {
+			data = append(data, str.String())
+			str.Reset()
+		}
+	}
+
+	for _, newline := range data {
+		reTitle := regexp.MustCompile(`(?U)## .\d \d* \(.*\)`)
+
+		title := reTitle.FindString((newline))
+
+		if title != "" {
+			fmt.Printf("%s\n", title)
+		}
+
+		re := regexp.MustCompile(`\*\*Time to repair\*\*: (\d*. \d*.|\d*.)`)
+		timeToRepair := 0
+		count := 0
+		for _, regmatch := range re.FindAllString(newline, -1) {
+			t := strings.Replace(regmatch, "**Time to repair**: ", "", -1)
+			timeToRepairTemp := convertToRaw(t)
+			timeToRepair = timeToRepair + timeToRepairTemp
+			count = count + 1
+		}
+
+		re2 := regexp.MustCompile(`\*\*Time to resolve\*\*: (\d*. \d*.|\d*.)`)
+		timeToResolve := 0
+		resolveCount := 0
+		for _, resolveMatch := range re2.FindAllString(newline, -1) {
+			t := strings.Replace(resolveMatch, "**Time to resolve**: ", "", -1)
+			timeToResolveTemp := convertToRaw(t)
+			timeToResolve = timeToResolve + timeToResolveTemp
+			resolveCount = resolveCount + 1
+		}
+
+		if count != 0 {
+			meanTimeToRepair := timeToRepair / count
+			d := time.Duration(meanTimeToRepair) * time.Minute
+			hours := int(d.Hours())
+			minutes := int(d.Minutes()) % 60
+			fmt.Printf("Incidents:%2d\n", count)
+			fmt.Printf("Mean time to repair: %2dh %02dm\n", hours, minutes)
+		}
+
+		if resolveCount != 0 {
+			meantTimeToResolve := timeToResolve / resolveCount
+			d := time.Duration(meantTimeToResolve) * time.Minute
+			hours := int(d.Hours())
+			minutes := int(d.Minutes()) % 60
+			fmt.Printf("Mean time to resolve: %2dh %02dm", hours, minutes)
+			fmt.Println("\n")
+		}
+
+	}
+}
