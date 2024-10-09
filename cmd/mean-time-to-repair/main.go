702bda4f502ab97ec54dca3eec3fea00f7d05c31
Mike Bell <mike.bell1@digital.justice.gov.uk>
2024-10-09 10:08:34 +0100

    refactor: move variable to top of func

--- cmd/mean-time-to-repair/main.go
+++ cmd/mean-time-to-repair/main.go
@@ -12,13 +12,14 @@ import (
 )
 
 func readLines(path string) ([]string, error) {
+	var lines []string
+
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
